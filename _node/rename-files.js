472e1a67503994c4e945125d15e75a858907d15b
Александр Чертков <Aleksandr.Chertkov@leroymerlin.ru>
2024-06-14 11:16:14 +0500

    fix rerder rename fiels

--- _node/rename-files.js
+++ _node/rename-files.js
@@ -6,52 +6,59 @@ const path = require("path");
 const directoryPath = process.argv[2];
 
 // Перебираем содержимое директории
-fs.readdir(directoryPath, (error, files) => {
+fs.readdir(directoryPath, async (error, files) => {
   if (error) {
     console.error("Обка при чтении директории:", error);
     return;
   }
 
-  // Проходим по всем файлам и нумеруем их
-  files
-    .sort((a, b) => Math.abs(parseInt(a)) - Math.abs(parseInt(b)))
-    .forEach((file, index) => {
-      // Получаем полный к файлу
-      const filePath = path.join(directoryPath, file);
+  const tempFiles = [];
 
+  // отсортируем список для того чтобы установить старый порядок нумерации , если вдруг файлы в этой папке уэе были пронумерованы
+  files.sort((a, b) => Math.abs(parseInt(a)) - Math.abs(parseInt(b)));
+
+  for (const file of files) {
+    const index = files.indexOf(file);
+    // Получаем полный путь к файлу
+    const filePath = path.join(directoryPath, file);
+
+    try {
       // Проверяем, является ли файл обычным файлом, а не директорией
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          console.error("Ошибка при получении информации оле:", err);
-          return;
-        }
+      const stats = await fs.promises.stat(filePath);
+      if (!stats.isFile()) {
+        // Если это не файл, пропускаем его
+        continue;
+      }
+    } catch (error) {
+      console.error("Ошибка при чтении файла:", error);
+      continue;
+    }
+    try {
+      const tempFileName = `temp-${file}`;
+      await fs.promises.rename(
+        filePath,
+        path.join(directoryPath, tempFileName)
+      );
+      tempFiles.push(tempFileName);
+      console.log(`Файл "${file}" успешно переименован в "${tempFileName}"`);
+    } catch (error) {
+      console.error(`Ошибка при переименовании файла "${file}":`, error);
+      continue;
+    }
+  }
 
-        if (!stats.isFile()) {
-          // Если это не файл, пропускаем его
-          return;
-        }
+  // Проходим по всем файлам и нумеруем их
+  for (const file of tempFiles) {
+    const index = tempFiles.indexOf(file);
+    const filePath = path.join(directoryPath, file);
+    const newFileName = `${index}${path.extname(file)}`;
 
-        const newFileName = `${index}${path.extname(file)}`;
-
-        if (file !== newFileName) {
-          // Переименовываемл
-          fs.rename(
-            filePath,
-            path.join(directoryPath, newFileName),
-            (renameError) => {
-              if (renameError) {
-                console.error(
-                  `Ошибка при переименовании файла "${file}":`,
-                  renameError
-                );
-              } else {
-                console.log(
-                  `Файл "${file}" успешно переименован в "${newFileName}"`
-                );
-              }
-            }
-          );
-        }
-      });
-    });
+    try {
+      await fs.promises.rename(filePath, path.join(directoryPath, newFileName));
+      console.log(`Файл "${file}" успешно переименован в "${newFileName}"`);
+    } catch (error) {
+      console.error(`Ошибка при переименовании файла "${file}":`, error);
+      continue;
+    }
+  }
 });
